9c3ed7a6615a55dabc6e0363d03ad2bcf5b5a76b
Glen-Sasek-PCC-Student <glen.sasek+student@pcc.edu>
2026-05-20 03:01:16 +0000

    Working

--- main.cpp
+++ main.cpp
@@ -1,80 +1,103 @@
-// ------------- FILE HEADER -------------
-// Author ✅: 
-// Assignment ✅:
-// Date ✅:
-// Citations: 
-
-
-// ------------- ZYBOOKS SCORES -------------
-// Chapter ✅: 
-// Participation ✅: 
-// Challenge ✅:
-// Labs ✅:
-
-
-// ------------- DISCORD POSTS -------------
-// https://discord.com/invite/URYKKf8YHm
-// Count ✅:
-// Links (Optional): 
-
-
-// ------------- DESIGN DOCUMENT -------------
-// A. INPUT ✅: 
-// B. OUTPUT ✅:
-// C. CALCULATIONS ✅:
-// D. LOGIC and ALGORITHMS ✅:
-//    (Optional) flow chart link or file name: 
-
-
-// ------------- TESTING -------------
-// PASS ALL GIVEN SAMPLE RUN TESTS ✅: 
-// (Optional) Additional tests count:   
-
-
-// ------------- CODE -------------
 #include <iostream>
-
+#include <iomanip>
+#include <limits>
 using namespace std;
 
-// Function prototypes (if any)
+// Constants for menu choices
+const int ADULT_CHOICE = 1,
+          CHILD_CHOICE = 2,
+          SENIOR_CHOICE = 3,
+          QUIT_CHOICE = 4;
 
+// Constants for membership rates
+const double ADULT = 40.0,
+             CHILD = 20.0,
+             SENIOR = 30.0;
 
-// Main function
-// https://en.cppreference.com/w/cpp/language/main_function.html
-int main(int argc, char* argv[]) {
-  cout << "Hello, World!" << endl;
-  return 0;
+const int MIN_MONTHS = 0;
+const int MAX_MONTHS = 60;
+
+// Add function prototypes here
+void displayCharges(double charges);
+double choiceToUSD(int choice, int months);
+int getIntInRange(string prompt, int min, int max);
+void displayMenu();
+int getChoice();
+void handleChoice(int choice);
+
+int main() {
+    // Variables
+    int choice = 0;         // Menu choice
+    do {
+        displayMenu();
+        choice = getIntInRange("", ADULT_CHOICE, QUIT_CHOICE);
+        handleChoice(choice);
+    } while (choice != QUIT_CHOICE);
+
+    return 0;
 }
 
-// Function implementations (if any)
 
+void displayCharges(double charges) {
+    cout << fixed << showpoint << setprecision(2);
+    cout << "The total charges are $" << charges << endl;
+}
 
-// ------------- DESIGN -------------
-/* 
-Program Name:
+double choiceToUSD(int choice, int months) {
+    double usd = 0.0;
+    switch (choice) {
+    case ADULT_CHOICE:
+        usd = months * ADULT;
+        break;
+    case CHILD_CHOICE:
+        usd = months * CHILD;
+        break;
+    case SENIOR_CHOICE:
+        usd = months * SENIOR;
+    }
+    return usd;
+}
 
-Program Description:
+int getIntInRange(string prompt, int min, int max) {
+    int n = 0;
+    bool next = true;
+    while(next) {
+        cout << prompt;
+        cin >> n;
+        if(cin) {
+            if(min <= n and n <= max) {
+                next = false;
+            } 
+        } else {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
 
-Design:
-A. INPUT
-Define the input variables including name data type. 
+        if(next) {
+          cout << "ERROR: Out of range(" << min << ", " << max << ")" <<endl;
+        }
+    }
+    return n;
+}
 
-B. OUTPUT
-Define the output variables including data types. 
+void displayMenu() {
+    cout << "\n\t\tHealth Club Membership Menu\n\n"
+         << "1. Standard Adult Membership\n"
+         << "2. Child Membership\n"
+         << "3. Senior Citizen Membership\n"
+         << "4. Quit the Program\n\n"
+         << "Enter your choice: ";
+}
 
-C. CALCULATIONS
-Describe calculations used by algorithms in step D.  
-List all formulas. 
-If there are no calculations needed, state there are no calculations.
+void handleChoice(int choice) {
+    // Validate and process the user's choice.
+    if (choice == QUIT_CHOICE) {
+        return;
+    }
 
-D. LOGIC and ALGORITHMS
-Design the logic of your program using pseudocode or flowcharts. 
-Use conditionals, loops, functions or array constructs.
-List the steps in transforming inputs into outputs. 
-https://github.com/Glen-Sasek-PCC-Instructor/2025-06-22/blob/main/Pseudocode-Reference.txt
-
-
-SAMPLE RUNS
-Copy from assignment document.
-
-*/
+    int months = 0;         // Number of months
+    double charges_USD = 0.0;   // Monthly charges
+    months = getIntInRange("For how many months? ", MIN_MONTHS, MAX_MONTHS);
+    charges_USD = choiceToUSD(choice, months);
+    displayCharges(charges_USD);
+}
